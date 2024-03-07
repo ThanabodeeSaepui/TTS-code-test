a748f77f4df646739d6ad617147ee831867daa51
ThanabodeeSaepui <s6301012620138@email.kmutnb.ac.th>
2024-03-08 00:03:21 +0700

    Adjust detail page

--- frontend/src/components/Detail.tsx
+++ frontend/src/components/Detail.tsx
@@ -94,39 +94,54 @@ function Detail() {
   return (
     <>
       <h1 className="font-bold my-2 text-2xl">เจ้าของ</h1>
-      <div className="flex border-t-2 py-2">
-        <input type="text" placeholder="HN" className="input input-bordered flex items-center gap-2 " disabled 
-          value={HN}
-          onChange={(e) => {setHN(e.target.value)}}
-        />
-        <label className="input input-bordered flex items-center gap-2">
-          <svg xmlns="http://www.w3.org/3000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-          <input type="text" placeholder="ชื่อ" 
-            value={name}
-            onChange={(e) => {setName(e.target.value)}}
+      <div className="flex justify-center border-t-8 border-green-700 py-2">
+        <div className='flex justify-center items-center h-full'>
+          <h2>HN</h2>
+          <input type="text" placeholder="HN" className="input input-bordered flex items-center gap-2 mx-3" disabled 
+            value={HN}
+            onChange={(e) => {setHN(e.target.value)}}
           />
-        </label>
-        <label className="input input-bordered flex items-center gap-2">
-          <svg xmlns="http://www.w3.org/3000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-          <input type="text" placeholder="นามสกุล" 
-            value={surname}
-            onChange={(e) => {setSurname(e.target.value)}}/>
-        </label>
+        </div >
+        <div className='flex justify-center items-center h-full'>
+          <h2>ชื่อ</h2>
+          <label className="input input-bordered flex items-center gap-2 mx-3">
+            <svg xmlns="http://www.w3.org/3000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+            <input type="text" placeholder="ชื่อ" 
+              value={name}
+              onChange={(e) => {setName(e.target.value)}}
+            />
+          </label>
+        </div>
+        <div className='flex justify-center items-center h-full'>
+          <h2>นามสกุล</h2>
+          <label className="input input-bordered flex items-center gap-2 mx-3">
+            <svg xmlns="http://www.w3.org/3000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+            <input type="text" placeholder="นามสกุล" 
+              value={surname}
+              onChange={(e) => {setSurname(e.target.value)}}/>
+          </label>
+        </div>
       </div>
-      <div className="flex my-1">
-        <input type="text" placeholder="เบอร์ติดต่อ" className="input input-bordered flex items-center gap-2" 
-          value={phone}
-          onChange={(e) => {setPhone(e.target.value)}}
-        />
-        <label className="input input-bordered flex items-center gap-2">
-          <svg xmlns="http://www.w3.org/3000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-          <input type="text" placeholder="อีเมล์" 
-            value={email}
-            onChange={(e) => {setEmail(e.target.value)}}
+      <div className="flex justify-center my-1">
+        <div className='flex justify-center items-center h-full'>
+          <h2>เบอร์ติดต่อ</h2>
+          <input type="text" placeholder="เบอร์ติดต่อ" className="input input-bordered flex items-center gap-2 mx-3" 
+            value={phone}
+            onChange={(e) => {setPhone(e.target.value)}}
           />
-        </label>
+        </div>
+        <div className='flex justify-center items-center h-full'>
+          <h2>อีเมล์</h2>
+          <label className="input input-bordered flex items-center gap-2 mx-3">
+            <svg xmlns="http://www.w3.org/3000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+            <input type="text" placeholder="อีเมล์" 
+              value={email}
+              onChange={(e) => {setEmail(e.target.value)}}
+            />
+          </label>
+        </div>
       </div>
-      <div className="flex border-b-2 py-2">
+      <div className="flex justify-center border-b-8 border-green-700 py-2 space-x-1.5">
         <button className="btn" onClick={()=> window.open("/", '_parent')}>
           Cancel
         </button>
